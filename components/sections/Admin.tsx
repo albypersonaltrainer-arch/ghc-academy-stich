d06d7ce2b33b3bad0082bac93d595ad9f2472e44
albypersonaltrainer-arch <albypersonaltrainer@gmail.com>
2026-04-29 14:53:07 +0200

    Create Admin.tsx

---
+++ components/sections/Admin.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+export const AdminSection = () => (
+  <section className="p-10 text-left min-h-screen">
+    <h2 className="text-3xl font-bold mb-8 text-orange-600">PANEL DE CONTROL</h2>
+    <p>Gestión de contenidos y alumnos.</p>
+  </section>
+);
