24b9a214eb66d4023d21db1de699755a3769f3a6
albypersonaltrainer-arch <albypersonaltrainer@gmail.com>
2026-04-29 13:26:40 +0200

    Create layout.tsx

---
+++ app/layout.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+export const metadata = {
+  title: 'GHC Academy',
+  description: 'Sport Through Science',
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="es">
+      <body style={{ margin: 0 }}>{children}</body>
+    </html>
+  )
+}
