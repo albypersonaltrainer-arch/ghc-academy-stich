3454e81905c22628ab15e6ab1253ca7a1d45ad62
albypersonaltrainer-arch <albypersonaltrainer@gmail.com>
2026-05-01 14:18:26 +0200

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: true, // Usa el compilador SWC de alta velocidad escrito en Rust
-  images: {
-    domains: ['images.unsplash.com', 'res.cloudinary.com'], // Seguridad para imágenes externas
-  },
 };
 
 export default nextConfig;
