3aa7e99a95d1b606bbde15b09f5676cc588d462b
albypersonaltrainer-arch <albypersonaltrainer@gmail.com>
2026-05-01 16:50:17 +0200

    Delete components directory

--- components/common/Footer.tsx
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-export const Footer = ({ slogan }: any) => (
-  <footer className="p-10 border-t border-zinc-900 text-center text-[10px] text-zinc-600 tracking-widest">
-    © 2026 GHC ACADEMY | {slogan}
-  </footer>
-);
