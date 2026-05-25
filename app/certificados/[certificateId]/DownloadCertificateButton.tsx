56a36355258ca49aadc344f41efc9baec7dcd3b3
albypersonaltrainer-arch <albypersonaltrainer@gmail.com>
2026-05-25 14:30:36 +0200

    Update DownloadCertificateButton.tsx

--- app/certificados/[certificateId]/DownloadCertificateButton.tsx
+++ app/certificados/[certificateId]/DownloadCertificateButton.tsx
@@ -1,1 +1,13 @@
+'use client';
 
+export default function DownloadCertificateButton() {
+  function handleDownload() {
+    window.print();
+  }
+
+  return (
+    <button type="button" className="download-diploma-button" onClick={handleDownload}>
+      Descargar diploma
+    </button>
+  );
+}
