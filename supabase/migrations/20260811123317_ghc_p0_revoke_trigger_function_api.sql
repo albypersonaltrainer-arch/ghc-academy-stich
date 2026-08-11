bc7d1475acc35c7a90ef14597253c090c2056162
albypersonaltrainer-arch <albypersonaltrainer@gmail.com>
2026-08-11 14:50:42 +0200

    db: version trigger API revocations

---
+++ supabase/migrations/20260811123317_ghc_p0_revoke_trigger_function_api.sql
@@ -0,0 +1,3 @@
+revoke execute on function public.ghc_guard_exam_publication() from public, anon, authenticated;
+revoke execute on function public.ghc_unpublish_exam_when_question_unreviewed() from public, anon, authenticated;
+revoke execute on function public.ghc_guard_exam_attempt_insert() from public, anon, authenticated;
