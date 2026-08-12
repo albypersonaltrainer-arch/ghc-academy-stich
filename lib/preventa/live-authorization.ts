8fc6bc9c9c2d0880ce209aa495cf31cbc006452d
albypersonaltrainer-arch <albypersonaltrainer@gmail.com>
2026-08-12 16:32:14 +0200

    feat: registrar autorización versionada de SumUp LIVE

---
+++ lib/preventa/live-authorization.ts
@@ -0,0 +1,6 @@
+export const PREVENTA_SUMUP_LIVE_AUTHORIZATION = Object.freeze({
+  productionAuthorized: true,
+  checkoutEnabled: true,
+  webhookEnabled: true,
+  authorizedOn: '2026-08-12',
+});
