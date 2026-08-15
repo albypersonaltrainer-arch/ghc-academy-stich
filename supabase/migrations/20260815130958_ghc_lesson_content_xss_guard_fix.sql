e94a3bef2322634451a03588ddf0b4f4403f6eb2
albypersonaltrainer-arch <albypersonaltrainer@gmail.com>
2026-08-15 15:10:34 +0200

    security: corregir regex PostgreSQL del guard XSS

---
+++ supabase/migrations/20260815130958_ghc_lesson_content_xss_guard_fix.sql
@@ -0,0 +1,18 @@
+create or replace function private.ghc_guard_lesson_content_xss()
+returns trigger
+language plpgsql
+security definer
+set search_path = public, private
+as $$
+declare
+  v_content text := coalesce(new.content, '');
+begin
+  if v_content ~* '(<[[:space:]]*(script|iframe|object|embed|svg|math|form|input|button|textarea)([[:space:]>\/])|on[a-z]+[[:space:]]*=|javascript[[:space:]]*:|srcdoc[[:space:]]*=|data[[:space:]]*:[[:space:]]*text/html)' then
+    raise exception 'Contenido de lección rechazado por política de seguridad HTML.';
+  end if;
+
+  return new;
+end;
+$$;
+
+revoke all on function private.ghc_guard_lesson_content_xss() from public, anon, authenticated;
