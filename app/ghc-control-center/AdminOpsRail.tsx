5c24c7448018c59258537875d217c71ca96f3e69
albypersonaltrainer-arch <albypersonaltrainer@gmail.com>
2026-08-15 15:02:37 +0200

    security: enlazar configuración MFA desde Control Center

--- app/ghc-control-center/AdminOpsRail.tsx
+++ app/ghc-control-center/AdminOpsRail.tsx
@@ -38,6 +38,7 @@ export default function AdminOpsRail() {
     >
       <Link href="/ghc-control-center/accesos" style={linkStyle}>Pagos y accesos</Link>
       <Link href="/ghc-control-center/soporte" style={linkStyle}>Soporte</Link>
+      <Link href="/ghc-control-center/mfa" style={linkStyle}>Seguridad MFA</Link>
     </nav>
   )
 }
